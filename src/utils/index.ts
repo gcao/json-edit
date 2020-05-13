5eb04596fd8d1314e21562cfef166f5c528440d9
Guoliang Cao <gcao99@gmail.com>
2020-05-13 11:50:13 -0400

    Issue #2: get json from url - support proxy

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,21 @@
+const PROXY = "https://yacdn.org/serve/";
+
 export default class LocationHandler {
   public hasJson: boolean = false;
   private jsonUrl?: string;
+  public useProxy: boolean = false;
 
   constructor() {
     const url = new URL(window.location.toString());
-    const jsonUrl = url.searchParams.get('json-url');
+    const proxy = url.searchParams.get('proxy');
+    this.useProxy = proxy === '1' || proxy === 'true';
+
+    let jsonUrl = url.searchParams.get('json-url');
     if (jsonUrl) {
       this.hasJson = true;
+      if (this.useProxy) {
+        jsonUrl = `${PROXY}${jsonUrl}`;
+      }
       this.jsonUrl = jsonUrl;
     }
   }
