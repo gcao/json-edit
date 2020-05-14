4e1d794c861fdcc2f9b63c32610e71742c5b871d
Guoliang Cao <gcao99@gmail.com>
2020-05-14 19:47:42 -0400

    Issue #14 WIP: config for the editor

--- src/jconfig/index.test.ts
+++ src/jconfig/index.test.ts
@@ -1,5 +1,11 @@
 import JConfig from './index';
 
-test('JConfig should work', () => {
-  const config = new JConfig();
+test('fromJson should work', () => {
+  const config = JConfig.fromJson(`{
+    "first": true,
+    "second": []
+  }`);
+  expect(config.root.type).toEqual("object");
+  expect(config.root.props.first.type).toEqual("boolean");
+  expect(config.root.props.second.type).toEqual("array");
 });
--- src/jconfig/index.ts
+++ src/jconfig/index.ts
@@ -1,2 +1,43 @@
+import * as R from "ramda";
+
+export class ConfigItem {
+  public type?: any;
+  _props?: any;
+
+  get props() {
+    if (!this._props) {
+      this._props = {};
+    }
+    return this._props;
+  }
+
+  constructor() {
+  }
+}
+
 export default class JConfig {
+  public root: ConfigItem;
+
+  constructor() {
+    this.root = new ConfigItem();
+  }
+
+  public static fromJson(json: string): JConfig {
+    const config = new JConfig();
+    const parsed = JSON.parse(json);
+    this.dataToConfig(parsed, config.root);
+    return config;
+  }
+
+  static dataToConfig(data: any, config: ConfigItem) {
+    config.type = R.is(Array, data) ? "array" : typeof (data);
+
+    if (config.type === "object") {
+      for (let [key, value] of Object.entries(data)) {
+        let childConfig = new ConfigItem();
+        config.props[key] = childConfig;
+        this.dataToConfig(value, childConfig);
+      }
+    }
+  }
 }
