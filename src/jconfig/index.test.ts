8c1d6a8beda1bdd4092645a8e5f8cc79f61f694c
Guoliang Cao <gcao99@gmail.com>
2020-05-14 21:39:52 -0400

    Issue #14 WIP: config for the editor

--- src/jconfig/index.test.ts
+++ src/jconfig/index.test.ts
@@ -1,11 +1,30 @@
 import JConfig from './index';
 
-test('fromJson should work', () => {
-  const config = JConfig.fromJson(`{
+test('JConfig.fromJson() should work', () => {
+  let config = JConfig.fromJson(`{
     "first": true,
-    "second": []
+    "second": [],
+    "third": [1],
+    "string": "something"
   }`);
   expect(config.root.type).toEqual("object");
   expect(config.root.props.first.type).toEqual("boolean");
   expect(config.root.props.second.type).toEqual("array");
+  expect(config.root.props.third.children.type).toEqual("number");
+  expect(config.root.props.string.type).toEqual("string");
+});
+
+test('JConfig.deserialize() should work', () => {
+  let config = JConfig.fromJson(`{
+    "first": true,
+    "second": [],
+    "third": [1],
+    "string": "something"
+  }`);
+  config = JConfig.deserialize(config.toString());
+  expect(config.root.type).toEqual("object");
+  expect(config.root.props.first.type).toEqual("boolean");
+  expect(config.root.props.second.type).toEqual("array");
+  expect(config.root.props.third.children.type).toEqual("number");
+  expect(config.root.props.string.type).toEqual("string");
 });
--- src/jconfig/index.ts
+++ src/jconfig/index.ts
@@ -1,17 +1,62 @@
 import * as R from "ramda";
 
 export class ConfigItem {
-  public type?: any;
+  public type: string;
   _props?: any;
+  _children?: ConfigItem;
 
   get props() {
     if (!this._props) {
-      this._props = {};
+      this._props = new Map();
     }
     return this._props;
   }
 
+  get children(): ConfigItem {
+    if (!this._children) {
+      this._children = new ConfigItem();
+    }
+    return this._children;
+  }
+
+  static fromSerializable(o: any): ConfigItem {
+    let config = new ConfigItem();
+    config.type = o.type;
+    if (o.type === "array") {
+      config._children = this.fromSerializable(o.children);
+    } else if (o.type === "object") {
+      config._props = {} as any;
+      for (let [key, value] of Object.entries(o.props)) {
+        config._props[key] = this.fromSerializable(value);
+      }
+    }
+    return config;
+  }
+
+  public static deserialize(json: string): ConfigItem {
+    return this.fromSerializable(JSON.parse(json));
+  }
+
   constructor() {
+    this.type = "any";
+  }
+
+  toSerializable(): any {
+    let o: any = {};
+    o.type = this.type;
+    if (o.type === "array") {
+      o.children = this.children.toSerializable();
+    } else if (o.type === "object") {
+      o.props = {};
+      for (let [key, value] of Object.entries(this.props)) {
+        o.props[key] = (value as ConfigItem).toSerializable();
+      }
+    }
+    return o;
+  }
+
+  public toString() {
+    return JSON.stringify(this.toSerializable(), null, 2);
   }
 }
 
@@ -32,12 +77,29 @@ export default class JConfig {
   static dataToConfig(data: any, config: ConfigItem) {
     config.type = R.is(Array, data) ? "array" : typeof (data);
 
-    if (config.type === "object") {
+    if (config.type === "array") {
+      if (data.length > 0) {
+        this.dataToConfig(data[0], config.children);
+      }
+    } else if (data && config.type === "object") {
       for (let [key, value] of Object.entries(data)) {
         let childConfig = new ConfigItem();
         config.props[key] = childConfig;
         this.dataToConfig(value, childConfig);
       }
     }
+  }
+
+  public static deserialize(json: string): JConfig {
+    let config = new JConfig();
+    let parsed = JSON.parse(json);
+    config.root = ConfigItem.fromSerializable(parsed.root);
+    return config;
+  }
+
+  public toString(): string {
+    let o: any = {};
+    o.root = this.root.toSerializable();
+    return JSON.stringify(o, null, 2);
   }
 }
